c098737ad36f8bc741348af78687c4f3ead37244
selimreza777 <selimrezacse@gmail.com>
2025-08-13 13:41:31 +0600

    math.js

---
+++ math.js
@@ -0,0 +1,22 @@
+// const Math={
+//   min:function min(num1,num2,num3){
+//     return num2;
+//   }
+// }
+
+// console.log(Math.min);
+const min = Math.min(45, 1, 12, 64, 99, 126, 1, 0);
+console.log(min)
+const max = Math.max(12, 25, 45, 50, 80);
+console.log(max);
+console.log(Math.PI);
+console.log(Math.abs(-5))
+console.log(Math.round(2.83))
+console.log(Math.floor(2.93))
+console.log(Math.ceil(2.9999))
+console.log(Math.ceil(2.1))
+
+
+console.log(Math.random()*10)
+const rand = Math.round(Math.random()*10)
+console.log(rand)
